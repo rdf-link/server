f5eed8b803a7c3cb95246791bef0dfe19169518d
Matthieu Bosquet <matthieubosquet@gmail.com>
2024-07-31 19:17:06 +0100

    Iterate store for simplicity

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,4 @@
+import type { Term } from "@rdfjs/types";
 import { Readable } from "node:stream";
 import { DataFactory, Parser, Store } from 'n3';
 
@@ -16,16 +17,25 @@ const x = `
 <x1> 
     <y1> 'z1', 'z2' ;
     <y2> <z1>, <z2> .
-<a> <b> <c>, <d> .
+<a> <b> <c>, <d>, <x> .
 `;
 
-async function* concatStreams(readables: Readable[]) {
-    for (const readable of readables) {
-        for await (const chunk of readable) { yield chunk }
+function* describe(store: Store, term: Term) {
+    const blanks = [];
+    for (const quad of store) {
+        if (quad.subject.equals(term)) {
+            yield quad
+        }
+        if (quad.object.equals(term)) {
+            yield quad
+        }
+        // TODO check for blanknodes
+        // keep an array of subjects to check if the blank existed in what's been gone through
     }
 }
 
 export async function data(url: URL): Promise<Readable> {
+    // TODO try with streaming parser
     const parser = new Parser({ baseIRI: 'http://example.org/', format: 'Turtle' });
     const store = new Store(parser.parse((x)));
 
@@ -33,10 +43,17 @@ export async function data(url: URL): Promise<Readable> {
         throw new Error("TEST")
     }
 
-    const iterable = await concatStreams([
-        store.match(DataFactory.namedNode('http://example.org/x')),
-        store.match(DataFactory.namedNode('http://example.org/x1'))
-    ].map(x => Readable.from(x)))
+    // async function* concatStreams(readables: Readable[]) {
+    //     for (const readable of readables) {
+    //         for await (const chunk of readable) { yield chunk }
+    //     }
+    // }
+    // const iterable = await concatStreams([
+    //     store.match(DataFactory.namedNode('http://example.org/x')),
+    //     store.match(DataFactory.namedNode('http://example.org/x1'))
+    // ].map(x => Readable.from(x)))
+
+    const iterable = describe(store, DataFactory.namedNode('http://example.org/x'));
 
     // Create a readable stream from the turtle string
     return Readable.from(iterable);
